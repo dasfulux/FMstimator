baeb0f4100f5c8bc896b7cf8e98f6612188cebf9
dasfulux <github@felixwunn.de>
2026-03-18 12:22:43 +0100

    Remove old submodules

--- eval/curl_eval/scripts/curl_7.32.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-OUTPUT="output/v7_32"
-mkdir -p $OUTPUT
-../../../src/FMstimator.py ../repo ../feature_models/fm_v7.30.xml ../feature_models/fm_v7.40.xml curl-7_32_0 --clangarg=-Ilib --clangarg=-Iinclude -vvv --output $OUTPUT 2>$OUTPUT/err.log 1>$OUTPUT/out.txt
-
